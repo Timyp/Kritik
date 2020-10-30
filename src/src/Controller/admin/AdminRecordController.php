18eacd5e94dc53ea76a5bee8d89c178befe786d1
timy-p <sdecalogne@gmail.com>
2020-10-31 00:09:51 +0100

    modif dashboard 30/10

--- src/src/Controller/admin/AdminRecordController.php
+++ src/src/Controller/admin/AdminRecordController.php
@@ -4,12 +4,45 @@
 namespace App\Controller\admin;
 
 
+use App\Repository\RecordRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/admin", name="admin_")
+ */
 class AdminRecordController extends AbstractController
 {
-    public function index()
-    {
+    /**
+     * @Route("_record", name="record")
+     * @param RecordRepository $recordRepository
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     */
+    public function index(
+        RecordRepository $recordRepository,
+        Request $request,
+        PaginatorInterface $paginator
+    ){
+        //Get records list
+        $labels = array_reverse($recordRepository->findAll());
 
+//        foreach($labels as $label) {
+//            dd($label->getArtist()->getName());
+//        }
+
+        $pagination = $paginator->paginate(
+            $labels,
+            $request->query->getInt('page', 1),
+            10
+        );
+
+
+        return $this->render('admin/dashboard_record_index.html.twig', [
+            'pagination' => $pagination,
+        ]);
     }
 }
